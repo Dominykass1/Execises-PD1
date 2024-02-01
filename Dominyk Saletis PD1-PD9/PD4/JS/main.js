fbf831e0cb1032d6ecfa35dedca7f4c855bd34e7
Dominykass1 <150919957+Dominykass1@users.noreply.github.com>
2024-02-01 11:57:56 +0200

    Add files via upload

---
+++ Dominyk Saletis PD1-PD9/PD4/JS/main.js
@@ -0,0 +1,15 @@
+let petName;
+let petAge;
+
+petName = "suo";
+petAge = 7;
+
+console.log("Pet Name: " + petName);
+console.log("Pet Age: " + petAge);
+
+petName = "yuki";
+petAge = 4;
+
+const buildingAddress = "Gatve 1";
+
+console.log("Building Address: " + buildingAddress);
